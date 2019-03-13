c9052f91d25eaad4c787733851ff513182a4cd2d
precompiler <lishengyin.msn@hotmail.com>
2019-03-13 20:51:04 +0000

    Customize SpringApplication

---
+++ get-started/src/main/java/com/github/precompiler/customize/NoBannerApplication.java
@@ -0,0 +1,16 @@
+package com.github.precompiler.customize;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(NoBannerApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+
+}
